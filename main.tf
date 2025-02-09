e5cf2ed6f359d15a1818477cdbb8a7df33633f36
rfgiacomini <rafranca2005@hotmail.com>
2025-02-08 21:19:22 -0300

    alteracao de arquivo main

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.82.2"
+      version = "5.16.2"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "rfgiacomini"
+      managed-by = "terraform"
+    }
+  }
 }
