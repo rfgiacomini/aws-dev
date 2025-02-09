91866b969fb7fafadd835fa18fefdc677c44e3e3
rfgiacomini <rafranca2005@hotmail.com>
2025-02-09 01:26:04 -0300

    readequacao de codigo

--- modules/network/prd-sae1-vpc-services-1a/aws_vpc.tf
+++ modules/network/prd-sae1-vpc-services-1a/aws_vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "aws_vpc" {
+resource "aws_vpc" "aws_name_vpc" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -12,7 +12,7 @@ resource "aws_vpc" "aws_vpc" {
 }
 
 resource "aws_subnet" "aws_subnet" {
-  vpc_id                  = aws_vpc.aws_vpc.id
+  vpc_id                  = aws_vpc.aws_name_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 6, 2)
   availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = false
