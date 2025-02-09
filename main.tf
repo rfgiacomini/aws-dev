8e69f839bd8f4c9a24f7b06ac14264862bab2486
rfgiacomini <rafranca2005@hotmail.com>
2025-02-09 01:10:45 -0300

    readequacao de codigo

--- main.tf
+++ main.tf
@@ -4,12 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.86.0"
     }
-  }
-  backend "s3" {
-    bucket = "terraform-aws-version"
-    key    = "aws-ec2-modules/terraform.tfstate"
-    region = "us-east-1"
-  }
+
 }
 
 provider "aws" {
