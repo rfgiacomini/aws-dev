c6426a5ea50b8c2618260aab61e8832fa50bb997
rfgiacomini <rafranca2005@hotmail.com>
2025-02-09 14:55:01 -0300

    Criando as VPS

--- modules.tf
+++ modules.tf
@@ -2,7 +2,7 @@ module "prd-sae1-vpc-services-1a" {
   source = "./modules/network/prd-sae1-vpc-services-1a"
   tags   = local.tags
 }
-module "prd-sae1-ec2-datacenter" {
-  source = "./modules/ec2/prd-sae1-ec2-datacenter"
-  tags   = local.tags
-}
+#module "prd-sae1-ec2-datacenter" {
+#  source = "./modules/ec2/prd-sae1-ec2-datacenter"
+#  tags   = local.tags
+#}
