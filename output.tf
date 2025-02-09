999ca9c9c95a9c9ee29e9b6e644f28d7c8d1df85
rfgiacomini <rafranca2005@hotmail.com>
2025-02-08 23:25:58 -0300

    Feature add Ec2-datacenter

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "aws_ec2_vpc_name_1a" {
+  value = module.prd-sae1-vpc-services-1a.aws_vpc_name_1a
+}
+output "aws_ec2_vpc_name_1b" {
+  value = module.prd-sae1-vpc-services-1b.aws_vpc_name_1b
+}
+output "aws_ec2_vpc_name_1c" {
+  value = module.prd-sae1-vpc-services-1c.aws_vpc_name_1c
+}
