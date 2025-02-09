87834f002cb3ec356ebd2e944a0b0f724832862a
rfgiacomini <rafranca2005@hotmail.com>
2025-02-09 01:01:21 -0300

    readequacao de codigo

--- modules.tf
+++ modules.tf
@@ -2,14 +2,7 @@ module "prd-sae1-vpc-services-1a" {
   source = "./modules/network/prd-sae1-vpc-services-1a"
   tags   = local.tags
 }
-module "prd-sae1-vpc-services-1b" {
-  source = "./modules/network/prd-sae1-vpc-services-1b"
-  tags   = local.tags
-}
-module "prd-sae1-vpc-services-1c" {
-  source = "./modules/network/prd-sae1-vpc-services-1c"
-  tags   = local.tags
-}
+
 module "prd-sae1-ec2-datacenter" {
   source = "./modules/ec2/prd-sae1-ec2-datacenter"
   tags   = local.tags
--- modules/network/prd-sae1-vpc-services-1b/output.tf
+++
@@ -1,3 +0,0 @@
-output "aws_vpc_name_1b" {
-  value = aws_vpc.aws_vpc_name.id
-}
