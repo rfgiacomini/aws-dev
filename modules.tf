ff5b7b6c50e8caef5ac42e4d4b12039eef470d3f
rfgiacomini <rafranca2005@hotmail.com>
2025-02-11 23:00:21 -0300

    Criancao de novo ambiente

--- modules.tf
+++ modules.tf
@@ -19,5 +19,8 @@ module "prd-sae1-vpc-services-1b" {
 #}
 
 module "aws_profixlist_google" {
-  source = "./modules/google/prefix-list"
+  source = "./modules/prefix-list/google/prefix-list"
+}
+module "aws_profixlist_escritorio" {
+  source = "./modules/prefix-list/escritorio/prefix-list"
 }
